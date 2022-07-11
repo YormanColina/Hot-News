5ae2a8d55dfc7676381d761711a4f98f381ec564
Yorman Colina <yorman.colina1997@gmail.com>
2022-07-11 12:10:15 -0500

    remove pod folder

--- Pods/Target Support Files/Pods-Hot-News-Hot-NewsUITests/Pods-Hot-News-Hot-NewsUITests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_Hot_News_Hot_NewsUITests : NSObject
-@end
-@implementation PodsDummy_Pods_Hot_News_Hot_NewsUITests
-@end
--- Pods/Target Support Files/Pods-Hot-News/Pods-Hot-News-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_Hot_News : NSObject
-@end
-@implementation PodsDummy_Pods_Hot_News
-@end

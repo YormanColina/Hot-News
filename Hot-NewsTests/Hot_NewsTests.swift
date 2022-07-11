8fb7a2c3b0a7424b0885fe08b1959a045f4dd887
Yorman Colina <yorman.colina1997@gmail.com>
2022-07-11 09:49:04 -0500

    reeemplazar path del proyecto

---
+++ Hot-NewsTests/Hot_NewsTests.swift
@@ -0,0 +1,25 @@
+//
+//  Hot_NewsTests.swift
+//  Hot-NewsTests
+//
+//  Created by Apple on 8/07/22.
+//
+
+import XCTest
+@testable import Hot_News
+
+class Hot_NewsTests: XCTestCase {
+    
+    let homeViewModel: ViewModelHomeProtocol = ViewModelHome()
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+  
+
+}
